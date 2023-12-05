ce3d530611f7b5bfc2152f9c232293f6c12442bb
funizo <k4160621@naver.com>
2023-12-05 18:27:18 +0900

    로그인일때만 글쓰기 구현

--- pandaproject/src/components/bookContent/bookContent.js
+++ pandaproject/src/components/bookContent/bookContent.js
@@ -5,72 +5,83 @@ import Skeleton from "react-loading-skeleton";
 import Toolbar from "../toolbar/toolbar";
 import "./bookContent.css";
 import Footer from "../footer/footer";
+import jwt_decode from "jwt-decode";
 
 function NovelContent() {
 
-  const navigate  = useNavigate();
-  const [loading, setLoading] = useState(true);
-  const [bookData, setBookData] = useState([]);
+    const navigate  = useNavigate();
+    const [loading, setLoading] = useState(true);
+    const [bookData, setBookData] = useState([]);
+    const [userInfo, setUserInfo] = useState(null);
+    
+    useEffect(() => {
+        const token = localStorage.getItem('token');
 
-  useEffect(() => {
-      const timer = setTimeout(() => {
-          setLoading(false);
-      }, 300);
-      return () => clearTimeout(timer);
-  }, []);
+        if (token) {
+        // 토큰이 존재할 경우 디코드하여 사용자 정보 설정
+            const decoded = jwt_decode(token);
+            setUserInfo(decoded);
+        } 
+    }, []);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 300);
+        return () => clearTimeout(timer);
+    }, []);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch('http://localhost:8080/category/book');
-        if (!res.ok) {
-          throw new Error('서버 응답 실패');
+    useEffect(() => {
+        const fetchData = async () => {
+        try {
+            const res = await fetch('http://localhost:8080/category/book');
+            if (!res.ok) {
+            throw new Error('서버 응답 실패');
+            }
+            const data = await res.json();
+            setBookData(data.result);
+            console.log(bookData);
+        } catch (error) {
+            console.error('데이터를 불러오는 중 에러 발생:', error);
         }
-        const data = await res.json();
-        setBookData(data.result);
-        console.log(bookData);
-      } catch (error) {
-        console.error('데이터를 불러오는 중 에러 발생:', error);
-      }
-    };
+        };
 
-    fetchData();
-  }, []);
+        fetchData();
+    }, []);
 
-  const goToBookDetail = (id, bookData) => {
-      navigate(`/category/book/detail/${id}`, { state: { bookData } });
-      console.log('bookData',id)
-  }
+    const goToBookDetail = (id, bookData) => {
+        navigate(`/category/book/detail/${id}`, { state: { bookData } });
+        console.log('bookData',id)
+    }
 
-  return(
-      
-      <div className='novel'>
-          <Toolbar/>
-          <h2>도서</h2>
-          {loading ? (
-              bookData.map((_, i) => (
-                  <div className="image-container" key={i}>
-                      <TabContentSkeleton />
-                  </div>
-      ))
-  ) : (
-      <div className="novel-container">
-      {bookData.map((a, i) => (
-          <div className="image-container" key={i} onClick={() => goToBookDetail(a.id, a)}>
-          {/* <Link to={`././detail/${a.id}`} key={a.id}> */}
-              <TabContent key={i} bookData={bookData[i]} i={i} />
-          {/* </Link> */}
-      </div>
-      
-      ))}
-          </div>
-      )}
-      <a href="/bookregister" className="write">글쓰기</a>
-      
-      <Footer/>
-  </div>
-  
-  )
+    return(
+        
+        <div className='novel'>
+            <Toolbar/>
+            <h2>도서</h2>
+            {loading ? (
+                bookData.map((_, i) => (
+                    <div className="image-container" key={i}>
+                        <TabContentSkeleton />
+                    </div>
+        ))
+    ) : (
+        <div className="novel-container">
+        {bookData.map((a, i) => (
+            <div className="image-container" key={i} onClick={() => goToBookDetail(a.id, a)}>
+            {/* <Link to={`././detail/${a.id}`} key={a.id}> */}
+                <TabContent key={i} bookData={bookData[i]} i={i} />
+            {/* </Link> */}
+        </div>
+        
+        ))}
+            </div>
+        )}
+        <a href={userInfo === null ? null : '/bookregister'} onClick={() => userInfo === null && alert("로그인해주세요")} className="write">글쓰기</a>
+        
+        <Footer/>
+    </div>
+    
+    )
 }
 
 
