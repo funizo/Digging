dd9f7ade1fa2bbb4e30d2d09774bee80039e54f3
funizo <k4160621@naver.com>
2023-12-05 17:21:03 +0900

    카테고리이미지추가

---
+++ pandaproject/src/components/novelContent/bookContent.js
@@ -0,0 +1,111 @@
+import { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import "react-loading-skeleton/dist/skeleton.css";
+import Skeleton from "react-loading-skeleton";
+import Toolbar from "../toolbar/toolbar";
+import "./bookContent.css";
+import Footer from "../footer/footer";
+
+function NovelContent() {
+
+  const navigate  = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [bookData, setBookData] = useState([]);
+
+  useEffect(() => {
+      const timer = setTimeout(() => {
+          setLoading(false);
+      }, 300);
+      return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('http://localhost:8080/category/book');
+        if (!res.ok) {
+          throw new Error('서버 응답 실패');
+        }
+        const data = await res.json();
+        setBookData(data.result);
+        console.log(bookData);
+      } catch (error) {
+        console.error('데이터를 불러오는 중 에러 발생:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const goToBookDetail = (id, bookData) => {
+      navigate(`/category/book/detail/${id}`, { state: { bookData } });
+      console.log('bookData',id)
+  }
+
+  return(
+      
+      <div className='novel'>
+          <Toolbar/>
+          <h2>도서</h2>
+          {loading ? (
+              bookData.map((_, i) => (
+                  <div className="image-container" key={i}>
+                      <TabContentSkeleton />
+                  </div>
+      ))
+  ) : (
+      <div className="novel-container">
+      {bookData.map((a, i) => (
+          <div className="image-container" key={i} onClick={() => goToBookDetail(a.id, a)}>
+          {/* <Link to={`././detail/${a.id}`} key={a.id}> */}
+              <TabContent key={i} bookData={bookData[i]} i={i} />
+          {/* </Link> */}
+      </div>
+      
+      ))}
+          </div>
+      )}
+      <a href="/bookregister" className="write">글쓰기</a>
+      
+      <Footer/>
+  </div>
+  
+  )
+}
+
+
+
+
+
+
+function TabContentSkeleton() {
+  return (
+      <div className="skeleton-container">
+          <h3 className="skeleton-title"><Skeleton duration={1} width={300} height={100} /></h3>
+          <h3 className="skeleton-title"><Skeleton duration={1} width={300} height={20} /></h3>
+          <p className="skeleton-text"><Skeleton  duration={1} width={80} height={15} /></p>
+          <p className="skeleton-text-small"><Skeleton  duration={1} width={60} height={15} /></p>
+      </div>
+  )
+}
+
+function TabContent(props,i) {
+    
+      return (
+          <div className='novel-item'>
+              <div className='novel-img-box'>
+              <img src={props.bookData.bookImg} alt="" />
+              </div>
+              <div className='text-content'>
+              <p className='novel-card-title'>{props.bookData.bookTitle}</p>
+              <p className='novel-card-writer'>{props.bookData.username}</p>
+              <p className='novel-card-price'>{Number(props.bookData.price).toLocaleString()}원</p>
+              
+              </div>
+          </div>
+          
+      )
+  
+}
+
+export default NovelContent
--- pandaproject/src/data/popularItem.js
+++ pandaproject/src/data/popularItem.js
@@ -1,44 +1,44 @@
 const popularItem = [
     {
         id : 1,
-        title: '데이터',
-        img: "https://placekitten.com/300/200"
+        title: '패션',
+        img:  "/img/category/fashion.jpg"
    
     },
     {
         id : 2,
-        title: '데이터',
-        img: "https://placekitten.com/300/200"
+        title: '전자제품',
+        img: "/img/category/electronic.jpg"
     },
     {
         id : 3,
-        title: '데이터',
-        img: "https://placekitten.com/300/200"
+        title: '장난감',
+        img: "/img/category/toy.jpg"
     },
     {
         id : 4,
-        title: '데이터',
-        img: "https://placekitten.com/300/200"
+        title: '굿즈',
+        img: "/img/category/goods.jpg"
     },
     {
         id : 5,
-        title: '데이터',
-        img: "https://placekitten.com/300/200"
+        title: '티켓/기프티콘',
+        img: "/img/category/ticket.jpg"
     },
     {
         id : 6,
-        title: '데이터',
-        img: "https://placekitten.com/300/200"
+        title: '도서',
+        img: "/img/category/books.jpg"
     },
     {
         id : 7,
-        title: '데이터',
-        img: "https://placekitten.com/300/200"
+        title: '개인 제작물',
+        img: "/img/category/creation.jpg"
     },
     {
         id : 8,
-        title: '데이터',
-        img: "https://placekitten.com/300/200"
+        title: '이 달의 이벤트',
+        img: "/img/category/event.jpg"
     },
     {
         id : 9,
